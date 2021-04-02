7bee70d495866f5eae7c1d905ade64849dadb4dd
vmcguire6377 <79176079+vmcguire6377@users.noreply.github.com>
2021-04-02 09:48:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,66 @@
 // Assignment code here
 userLength = function () {
 
-
-
   // Get references to the #generate element
   var generateBtn = document.querySelector("#generate")
   addEventListener('click', event => {
 
-    userLength = prompt("Please choose a password length between 8 and 128 characters.")
+    userLength = Number(prompt("Please choose a password length between 8 and 128 characters."));
+    console.log(userLength);
 
 
     // to evaluate whether the number chosen is between the parameters
+    var userLength = parseInt(userLength);
 
     if (userLength > 7 && userLength < 129) {
-      var charTypes = prompt("Would you like to include capital letters, lower case letter, numbers, or symbols?")
 
+      var includeCapitals = confirm("Would you like to include capital letters?");
+      var includeLowerCase = confirm("Would you like to include lower case letters?");
+      var includeNumbers = confirm("Would you like to include numbers?");
+      var includeSymbols = confirm("Would you like to include symbols?");
+
+      console.log(includeCapitals);
+      console.log(includeLowerCase);
+      console.log(includeNumbers);
+      console.log(includeSymbols);
+    }
+    else {
+      window.alert("You did not choose a valid character type. Please try again.")
+    }
+
+  })
+
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password", userLength);
+    var charSet = ""
+    passwordText.value = password
+    if (includeCapitals === true) {
+      charSet = charSet + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+    else if (includeLowerCase === true) {
+      charSet = charSet + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz"
+    }
+    else if (includeNumbers === true) {
+      charSet = charSet + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz" + "0123456789"
+    }
+    else if (includeSymbols === true) {
+      charSet = charSet + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz" + "0123456789" + "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
     }
 
 
-    else {
-      window.alert("Please make a valid entry. Please choose a password length between 8 and 128 characters.")
+    function randPassword() {
+      var randPassword = "";
+      for (var i = 0; i < userLength; i++) {
+        //picks a character within charSet at index of random number
+        randPassword += charSet.charAt(Math.floor(Math.random() * charSet.userLength));
+      }
+    
+      console.log(randPassword);
+      return randPassword;
     }
   }
-  )
-}
-
+}  
 
 userLength();
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-
-  var passwordText = document.querySelector("#password")
-  if (charTypes.capitals === true) {
-    var pwdChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  } else if (charTypes.lowerCase === true) {
-    var pwdChars = "abcdefghijklmnopqrstuvwxyz"
-  } else if (charTypes.numbers === ok) {
-    var pwdChars = "0123456789"
-  } else if (charTypes.symbols === ok) {
-    var pwdChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-  }
-}
-
-
-var userLength = 10;
-var randPassword = Array(userLength).fill(pwdChars).map(function (x) { return x[Math.floor(Math.random() * x.length)] }).join('');
-
-passwordText.value = password;
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-
